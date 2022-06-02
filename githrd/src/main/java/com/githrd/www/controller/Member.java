e5adfefc4d05befb0ad14118ccb19ae9e83f1437
y <y@DESKTOP-1E4JF9F>
2022-06-02 12:21:38 +0900

    d1

--- githrd/src/main/java/com/githrd/www/controller/Member.java
+++ githrd/src/main/java/com/githrd/www/controller/Member.java
@@ -1,12 +1,13 @@
 package com.githrd.www.controller;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
 
 import com.githrd.www.dao.MemberDao;
 import com.githrd.www.vo.MemberVO;
@@ -23,9 +24,24 @@ public class Member {
 		return "member/login";
 	}
 	
-	
 	@RequestMapping("/loginProc.blp")
 	public ModelAndView loginProc(MemberVO mVO, HttpSession session, ModelAndView mv) {
+		System.out.println("### 일반 사용자");
+		int cnt = mDao.getLogin(mVO);
+		String view = "";
+		if(cnt == 1) {
+			session.setAttribute("SID", mVO.getId());
+			view = "redirect:../main.blp";
+		} else {
+			view = "redirect:login.blp";
+		}
+		mv.setViewName(view);
+		return mv;
+	}
+	
+	@RequestMapping(path="/loginProc.blp", params="id=admin")
+	public ModelAndView adminLogin(MemberVO mVO, HttpSession session, ModelAndView mv) {
+		System.out.println("### 관리자");
 		
 		int cnt = mDao.getLogin(mVO);
 		String view = "";
@@ -44,5 +60,21 @@ public class Member {
 		session.removeAttribute("SID");
 		mv.setViewName("redirect:../main.blp");
 		return mv;
+	}
+	
+	@RequestMapping(path="/idCheck.blp", params="id")
+	@ResponseBody
+	public Map idCheck(String id) {
+		HashMap map = new HashMap();
+		String result = "NO";
+		
+		int cnt = mDao.getIdCnt(id);
+		
+		if(cnt == 0) {
+			result = "OK";
+		}
+		
+		map.put("result", result);
+		return map;
 	}
 }
